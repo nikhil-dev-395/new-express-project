e9362e377235bbe851675cdcccffa0ee046a9c4a
nikhil-dev-395 <nikhilw395@gmail.com>
2024-10-28 20:06:06 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel , hi this is updated by nikhil"));
+app.get("/", (req, res) => res.send("Express on Vercel , hi this is updated by nikhil guruji by coding with nikhil cghannel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
